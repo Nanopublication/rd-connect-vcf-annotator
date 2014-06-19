ef78cbfb66ecff31596eb29ceab44ef72c7bcce7
eelkevanderhorst <eelkevanderhorst@gmail.com>
2014-06-19 15:02:38 +0200

    Changed main class

--- script/src/TSS.py
+++ script/src/TSS.py
@@ -1,5 +1,11 @@
+__author__ = 'Rajaram Kaliyaperumal'
+__author__ = 'Mark Thompson'
+__author__ = 'Eelke van der Horst'
+
 class TSS():
-    def __init__(slf, start, end, chromosome):
+    def __init__(self, cageClusterURI, chromosome, start, end, orientation):
+        self.cageClusterURI = cageClusterURI
         self.start = start
         self.end = end
         self.chromosome = chromosome
+        self.orientation = orientation
